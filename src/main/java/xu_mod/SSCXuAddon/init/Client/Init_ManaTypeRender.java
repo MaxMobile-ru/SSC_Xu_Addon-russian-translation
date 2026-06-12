c9039d3f850145e0ff802f4bc2dea283ac7e25e1
xu233333 <18510478058@163.com>
2026-06-12 19:36:17 +0800

    试试Agnes的模型 折腾了2个多钟头 改一个ManaTypeRender注册表就得对话3回(第1回没参数 第2回参数瞎填 第3次才填对) 就当玩具用吧

--- src/main/java/xu_mod/SSCXuAddon/init/Client/Init_ManaTypeRender.java
+++ src/main/java/xu_mod/SSCXuAddon/init/Client/Init_ManaTypeRender.java
@@ -8,8 +8,8 @@ import xu_mod.SSCXuAddon.init.Init_ManaType;
 public class Init_ManaTypeRender {
     static {
         ManaRegistriesClient.registerManaTypeRender(Init_ManaType.FamiliarFoxPurifyMana, new FamiliarFoxPurifyManaRender());
-        ManaRegistriesClient.registerManaTypeRender(Init_ManaType.BatBloodResource, new BatBloodResourceRender());
-        ManaRegistriesClient.registerManaTypeRender(Init_ManaType.AllayResource, new AllayResourceRender());
+        ManaRegistriesClient.registerManaTypeRender(Init_ManaType.BatBloodResource, new DefaultResourceBarRender(SSCXuAddon.identifier("textures/gui/bat_blood_bar.png"), false, true, true, 0xFFCC0000));
+        ManaRegistriesClient.registerManaTypeRender(Init_ManaType.AllayResource, new DefaultResourceBarRender(SSCXuAddon.identifier("textures/gui/allay_bar.png"), false, true, true, 0xFF7F00FF));
         ManaRegistriesClient.registerManaTypeRender(Init_ManaType.OcelotStaminaResource, new StaminaResourceRender());
         ManaRegistriesClient.registerManaTypeRender(Init_ManaType.AxolotlWaterResource, new DefaultResourceBarRender(SSCXuAddon.identifier("textures/gui/water_bar.png"), false, true, true, 0xFF70F3FF));
     }
